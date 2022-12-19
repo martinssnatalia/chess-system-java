a10c5a2c2709f21bc0e9ce60c198a65b121c35a5
Natalia Martins <martinsds.natalia@gmail.com>
2022-12-19 05:58:21 -0300

    Class Position created

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Git test");
+
+		Position pos = new Position(3,5);
+		System.out.println(pos);
 
 	}
 
