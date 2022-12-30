4606084dd979f8e07c3ffb436e35d2b0b7e8020e
Natalia Martins <martinsds.natalia@gmail.com>
2022-12-29 22:19:51 -0300

    Board print improvement

--- src/application/Application.java
+++ src/application/Application.java
@@ -5,12 +5,8 @@ import chess.ChessMatch;
 public class Application {
 
 	public static void main(String[] args) {
-
+		
 		ChessMatch chessMatch = new ChessMatch();
-		
 		UI.printBoard(chessMatch.getPieces());
-		
-
 	}
-
 }
