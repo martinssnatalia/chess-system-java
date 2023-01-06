525ddf00257be3b6defe138bce682a237df904f8
Natalia Martins <martinsds.natalia@gmail.com>
2023-01-05 21:22:31 -0300

    cleaning screen after piece deslocation and handling with exceptions

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -9,22 +11,36 @@ import chess.ChessPosition;
 public class Application {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		
-		//keep the looping while don't have the checkmate logic implemented. 
+
+		// keep the looping while don't have the checkmate logic implemented.
 		while (true) {
-		UI.printBoard(chessMatch.getPieces());
-		System.out.print("Source: ");
-		ChessPosition source = UI.readChessPosition(sc);
-		
-		System.out.println();
-		System.out.print("Target: ");
-		ChessPosition target = UI.readChessPosition(sc);
-		
-		ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine(); // to wait until press 'enter'
+			}
+
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+
 		}
-	
-}
+
+	}
 }
