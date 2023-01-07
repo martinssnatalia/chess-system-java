71a98e604833097b9871433329999cdd3faeb494
Natalia Martins <martinsds.natalia@gmail.com>
2023-01-07 17:15:47 -0300

    Handling with captured pieces

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -14,12 +16,13 @@ public class Application {
 
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> capturedPieces = new ArrayList<>();
 
 		// keep the looping while don't have the checkmate logic implemented.
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, capturedPieces);
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
@@ -32,6 +35,10 @@ public class Application {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if (capturedPiece != null) {
+					capturedPieces.add(capturedPiece);
+				}
 			}
 
 			catch (ChessException e) {
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -12,6 +15,10 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> piecesOutOfBoard = new ArrayList<>();
+	
+	
 	public ChessMatch() {
 		board = new Board(8, 8);
 		turn = 1;
@@ -60,6 +67,11 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		
+		if (capturedPiece != null) {
+			piecesOnTheBoard.remove(capturedPiece);
+			piecesOutOfBoard.add(capturedPiece);
+		}
 		return capturedPiece;
 	}
 
@@ -88,6 +100,7 @@ public class ChessMatch {
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnTheBoard.add(piece);
 	}
 
 	private void initialSetup() {
