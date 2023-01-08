2dc185ea6b3c6b50e351c30765e3ce8bf4462973
Natalia Martins <martinsds.natalia@gmail.com>
2023-01-08 19:32:21 -0300

    check logic implementation

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -16,6 +16,10 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position); //converting matrix type position in chess position (letter and number)
+	}
 
 	protected boolean isThereOpponentPiece (Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
